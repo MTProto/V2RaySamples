466148fbb1a2fa21598f144eaf887a2df4c531b6
MTProto <52460888+MTProto@users.noreply.github.com>
2023-02-09 05:01:28 +0330

    Add files via upload

--- runner.sh
+++ runner.sh
@@ -4,7 +4,7 @@ pgrep -x phpbb
 # if not found - equals to 1, start it
 if [ $? -eq 1 ]
 then
-    ./phpbb run --config ./bridge.json 2>null 1>null
+    ./phpbb run --config ./bridge.json 2>/dev/null 1>/dev/null
 else
     echo "Is running" > isrunning.txt
 fi
